1faac49f1d418afe055ab44ed49b60503ea0aa8f
Geolykt <admin@geolykt.de>
2021-01-18 23:10:28 +0100

    Initial commit
    To 60 fps and beyond!

---
+++ src/main/java/de/geolykt/faststar/FastStar.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2021 Geolykt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.geolykt.faststar;
+
+import net.minestom.server.extensions.Extension;
+
+public class FastStar extends Extension {
+
+    @Override
+    public void initialize() {} // We don't need to do anything - the extension is purely mixin based
+
+    @Override
+    public void terminate() {}
+
+}
