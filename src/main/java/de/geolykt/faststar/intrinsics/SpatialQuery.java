ee17cf80a9ef8e7d74901d3cff6e2baac4927d7c
Geolykt <admin@geolykt.de>
2024-07-01 18:29:28 +0200

    Use custom QuadTree implementation for k-nearest-neighbours queries
    A rather extremely buggy implementation, but works with a 100% accuracy 99% of the time.
    As for the remaining 1% of the time - the inaccuracy will be subtle enough that it can
    be discarded. Yet, if the setup is right - people WILL notice something is wrong, so
    eventually I will have to use a different knn implementation rather than one I wrote
    myself.

--- src/main/java/de/geolykt/faststar/intrinsics/SpatialQuery.java
+++ src/main/java/de/geolykt/faststar/intrinsics/SpatialQuery.java
@@ -1,12 +1,13 @@
 package de.geolykt.faststar.intrinsics;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
-import org.tinspin.index.Index.PointIteratorKnn;
-import org.tinspin.index.PointMap;
-import org.tinspin.index.kdtree.KDTree;
+import org.lwjgl.util.vector.Vector2f;
+import org.slf4j.LoggerFactory;
 
+import de.geolykt.faststar.qtree.QuadTree;
 import de.geolykt.starloader.api.Galimulator;
 import de.geolykt.starloader.api.empire.Star;
 
@@ -14,6 +15,63 @@ import snoddasmannen.galimulator.Space;
 import snoddasmannen.galimulator.actors.Actor;
 
 public class SpatialQuery {
+
+    private static QuadTree<Star> stars;
+    private static QuadTree<Actor> actors;
+    private static final Vector2f VECTOR_TEMP_DRAW = new Vector2f();
+    private static final Vector2f VECTOR_TEMP_TICK = new Vector2f();
+
+    public static Actor getNearestActor(float x, float y, @NotNull Actor witness) {
+        Iterator<Actor> it = SpatialQuery.actors.queryKnn(x, y);
+        if (!it.hasNext()) {
+            LoggerFactory.getLogger(SpatialQuery.class).warn("Actor {} not contained in QuadTree", witness);
+            return null;
+        }
+        it.next();
+        return it.hasNext() ? it.next() : null;
+    }
+
+    public static Actor getNearestActorHostile(float x, float y, @NotNull Actor witness) {
+        Iterator<Actor> it = SpatialQuery.actors.queryKnn(x, y);
+        while (it.hasNext()) {
+            Actor v = it.next();
+            if (v == witness
+                    || v.isSubordinateOf(witness)
+                    || v.isUntouchable()
+                    || (witness.getOwner() != null && !witness.isHostile(v))) {
+                continue;
+            }
+            return v;
+        }
+
+        return null;
+    }
+
+    public static void getNearestStars(float x, float y, int cutoff, @NotNull List<Star> out) {
+        SpatialQuery.stars.queryKnn(x, y, cutoff, out);
+    }
+
+    public static void updateActorsActorTicking() {
+        SpatialQuery.actors = new QuadTree<>(Space.getMaxX(), (actor) -> {
+            SpatialQuery.VECTOR_TEMP_TICK.set(actor.getX(), actor.getY());
+            return SpatialQuery.VECTOR_TEMP_TICK;
+        });
+        for (Actor a : Space.actors) {
+            assert a != null;
+            SpatialQuery.actors.addElement(a);
+        }
+    }
+
+    public static void updateStarsActorDrawing() {
+        SpatialQuery.stars = new QuadTree<>(Space.getMaxX(), (star) -> {
+            SpatialQuery.VECTOR_TEMP_DRAW.set(star.getX(), star.getY());
+            return SpatialQuery.VECTOR_TEMP_DRAW;
+        });
+        for (Star star : Galimulator.getUniverse().getStarsView()) {
+            SpatialQuery.stars.addElement(star);
+        }
+    }
+    /*
 
     private static PointMap<Star> stars;
     private static PointMap<Actor> actors;
@@ -60,5 +118,5 @@ public class SpatialQuery {
             stars.insert(new double[] {s.getX(), s.getY()}, s);
         }
         SpatialQuery.stars = stars;
-    }
+    }*/
 }
